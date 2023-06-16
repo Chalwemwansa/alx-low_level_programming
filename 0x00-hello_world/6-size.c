44eb2d865a780e1efc399edf3c3fb051d20b5966
chalwemwansa <chalwemwansa23@gmail.com>
2023-06-15 20:12:43 -0700

    preprocessor

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@ int main(void)
 char c;
 int i;
 long l;
-long long ll
+long long ll;
 float f;
 printf("Size of a char: %lu byte(s)\nSize of an int: %lu byte(s)\nSize of a long int: %lu byte(s)\nSize of a long long int: %lu byte(s)\nSize of a float: %lu byte(s)", (unsigned long)sizeof(c), (unsigned long)sizeof(i), (unsigned long)sizeof(l), (unsigned long)sizeof(ll), (unsigned long)sizeof(f));
 return (0);
