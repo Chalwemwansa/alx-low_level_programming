b6d1b10da40daf70017772da90eb16bcb1b6a657
chalwemwansa <chalwemwansa23@gmail.com>
2023-06-15 19:59:31 -0700

    preprocessor

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main- program prints a line using the function printf 
- * Return:0
+ * main - program prints a line using the function printf
+ * Return: 0
  */
 int main(void)
 {
