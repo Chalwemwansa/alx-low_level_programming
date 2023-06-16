e73be2ffe6249c43f44b212e6a1e74bf785c986c
chalwemwansa <chalwemwansa23@gmail.com>
2023-06-15 19:53:36 -0700

    preprocessor

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - the code starts from the main
+ * Return: 0
+ */
+int main(void)/*where the code starts from is the main*/
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio>
+/**
+ * main-prints the various types of variables
+ * return: 0
+ */
+int main(void)
+{
+char c;
+int i;
+long l;
+long long ll
+float f;
+printf("Size of a char: %lu byte(s)\nSize of an int: %lu byte(s)\nSize of a long int: %lu byte(s)\nSize of a long long int: %lu byte(s)\nSize of a float: %lu byte(s)", c, i, l, ll, f);
+return (0);
+}
