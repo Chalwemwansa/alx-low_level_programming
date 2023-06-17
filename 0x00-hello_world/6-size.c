ec443921e3bc90701030b02a4072a7d9c8a787d0
root <root@CHALWEs-LAPTOP.localdomain>
2023-06-17 16:44:29 +0200

    preprocessor

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
-#include <stdio>
+#include <stdio.h>
 /**
- * main-prints the various types of variables
- * return: 0
- */
+*main - prints the various types of variables
+*Return: 0
+*/
 int main(void)
 {
 char c;
@@ -10,6 +10,6 @@ int i;
 long l;
 long long ll;
 float f;
-printf("Size of a char: %lu byte(s)\nSize of an int: %lu byte(s)\nSize of a long int: %lu byte(s)\nSize of a long long int: %lu byte(s)\nSize of a float: %lu byte(s)", (unsigned long)sizeof(c), (unsigned long)sizeof(i), (unsigned long)sizeof(l), (unsigned long)sizeof(ll), (unsigned long)sizeof(f));
+printf("Size of a char: %lu byte(s)\nSize of an int: %lu byte(s)\nSize of a long int: %lu byte(s)\nSize of a long long int: %lu byte(s)\nSize of a float: %lu byte(s)\n", (unsigned long)sizeof(c), (unsigned long)sizeof(i), (unsigned long)sizeof(l), (unsigned long)sizeof(ll), (unsigned long)sizeof(f));
 return (0);
 }
