dbcaa733ec8d4c3ce01c584e0ed1b4cfef2f399e
root <root@CHALWEs-LAPTOP.localdomain>
2023-06-28 01:22:51 +0200

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * _atoi - changes a string with numbers into an integer
+ * Return: 0
+ * @s: variable to be used in the string
+ */
+int _atoi(char *s)
+{
+int length = 0, total = 0, i, i1, value = 1;
+while (s[length] != '\0')
+length++;
+if (s[0] == '-')
+{
+for (i1 = length - 1; i1 > 0; i1--)
+{
+for (i = 0; i < i1; i++)
+value *= 10;
+unsigned int v = s[length - i1] * value;
+total -= v;
+}
+}
+if (s[0] == '+')
+{
+for (i1 = length - 1; i1 > 0; i1--)
+{
+for (i = 0; i < i1; i++)
+value *= 10;
+unsigned int v = s[length - i1] * value;
+total += v;
+}
+}
+else
+{
+for (i1 = length; i1 > 0; i1--)
+{
+for (i = 0; i < i1; i++)
+value *= 10;
+unsigned int v = s[length - i1] * value;
+total += v;
+}
+}
+return (total);
+}
