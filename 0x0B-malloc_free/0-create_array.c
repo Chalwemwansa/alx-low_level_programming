cfe32625eb6ea3708a5c52c80f7c187c00e015fe
root <root@CHALWEs-LAPTOP>
2023-07-11 13:47:17 +0200

    0x0B. C - malloc, free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,12 +10,12 @@
 char *create_array(unsigned int size, char c)
 {
 char *s;
+unsigned int i;
 s = malloc((size + 1) * sizeof(char));
-if (size == 0 || str == NULL)
+if (size == 0 || s == NULL)
 return (NULL);
 else
 {
-unsigned int i = 0;
 while (i < size)
 {
 s[i] = c;
