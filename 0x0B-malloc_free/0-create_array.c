fa1bbd483884f15da3447cdd0e17007e24976bc8
root <root@CHALWEs-LAPTOP>
2023-07-11 13:10:48 +0200

    0x0B. C - malloc, free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,10 +9,10 @@
  */
 char *create_array(unsigned int size, char c)
 {
-char *s = (char *)malloc(size);
+char *s =(char *)malloc(size);
 if (size == 0)
 	return (NULL);
-s [size] = '\0';
+s[size] = '\0';
 while (size >= 0)
 {
 s[size - 1] = c;
