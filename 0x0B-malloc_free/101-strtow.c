17ad9f62c8ff646ae9239fb33007da03ffddb5f7
root <root@CHALWEs-LAPTOP>
2023-07-11 22:58:59 +0200

    0x0B. C - malloc, free

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * strtow - splits string into words
+ * Return: 0
+ * @str: variable used in the code
+ */
+char **strtow(char *str)
+{
+char **s;
+if (str == NULL || str == "")
+	return (NULL);
+return (s);
+}
