846d7f8b5ca747eb32e1f8511792101355cf57f0
root <root@CHALWEs-LAPTOP>
2023-07-13 23:13:45 +0200

    0x0C. C - More malloc, free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,12 +14,12 @@ unsigned long i, p;
 if (nmemb == 0 || size == 0)
 	return (NULL);
 p = nmemb * size;
-s = (int *)malloc(p);
+s = malloc(p);
 if (s == NULL)
 	return (NULL);
 for (i = 0; i < p; i++)
 {
 s[i] = 0;
 }
-return ((void *)s);
+return (s);
 }
