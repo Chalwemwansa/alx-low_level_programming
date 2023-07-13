e1ac946fbdbfd017d51c799d029bc7a402bd8de7
root <root@CHALWEs-LAPTOP>
2023-07-14 01:09:22 +0200

    0x0C. C - More malloc, free

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * array_range - sets the string to integers
+ * Return: an int array
+ * @min: variable to be used in the code
+ * @max: variable used in the code
+ */
+int *array_range(int min, int max)
+{
+int *p;
+int i;
+if (min > max)
+	return (NULL);
+p = malloc(sizeof(int) * (max - min + 1));
+if (p == NULL)
+	return (NULL);
+for (i = 0; i < (max - min + 1); i++)
+{
+p[i] = min;
+min++;
+}
+return (p);
+}
