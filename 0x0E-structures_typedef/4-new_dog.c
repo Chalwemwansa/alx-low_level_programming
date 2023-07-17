6b10102814a125c460f61a2088c8fa68df48100a
chalwemwansa <chalwemwansa23@gmail.com>
2023-07-17 18:56:15 +0200

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * _strcp - copies strings
+ * @name: a copy of the name
+ * @owner: copy of owner
+ * @i:parameter
+ * @k:parameter
+ */
+void _strcp(int i, int k, char *name, char *owner)
+{
+char *ownercopy;
+char *namecopy;
+int i1, i2;
+while (name[i])
+	i++;
+while (owner[k])
+	k++;
+for (i1 = 0; (i1 <= i) && (name[i1] != '\0'); i1++)
+	namecopy[i1] = name[i1];
+while (i1 <= i)
+{
+	namecopy[i1] = '\0';
+	i1++;
+}
+for (i2 = 0; (i2 <= k) && (owner[k] != '\0'); i2++)
+ownercopy[i2] = owner[i2];
+while (i2 <= k)
+{
+ownercopy[i2] = '\0';
+i2++;
+}
+}
+/**
  * new_dog - creates a new dog
  * Return: character array
  * @name: variable
@@ -11,23 +42,12 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *p;
-int i = 0, k = 0;
-char *namecopy, *ownercopy;
-while (name[i])
-{
-namecopy[i] = name[i];
-i++;
-}
-while (owner[i])
-{
-ownercopy[k] = owner[k];
-k++;
-}
 p = malloc(sizeof(dog_t));
 if (p == NULL)
 	return (NULL);
 (*p).name = name;
 (*p).age = age;
 (*p).owner = owner;
+_strcp(0, 0, name, owner);
 return (p);
 }
