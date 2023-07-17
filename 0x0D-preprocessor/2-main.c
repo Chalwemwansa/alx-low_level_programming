ab32dc10d6a1dfdbacfc14c1a71f849ecadae6c9
root <root@CHALWEs-LAPTOP>
2023-07-17 14:23:58 +0200

    0x0D. C - Preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
+#pragma once
 /**
  * main - prints the name of the function
  * Return: 0
  */
-int main()
+int main(void)
 {
 printf("%s\n", __FILE__);
 return (0);
