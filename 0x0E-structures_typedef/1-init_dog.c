0a480db4da24243e8166631eb283d454a9bed77f
chalwemwansa <chalwemwansa23@gmail.com>
2023-07-17 16:50:12 +0200

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,2 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * init_dog - initialises a function of the type dev
+ * Return: void
+ * @d:variable used in hecode
+ * @name: variable used in the code
+ * @age:variable used in code
+ * @owner: owner of dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+struct dog *dog;
+dog = malloc(sizeof(struct dog));
+if (dog != NULL)
+{
+(*dog).name = name;
+(*dog).age = age;
+(*dog).owner = owner;
+}
+}
