99a0bff14a664186f5a91ff72a735361ef48c45c
chalwemwansa <chalwemwansa23@gmail.com>
2023-07-17 19:17:36 +0200

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,37 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * _strcp - copies strings
- * @name1: a copy of the name
- * @owner1: copy of owner
- * @i:parameter
- * @k:parameter
- */
-void _strcp(int i, int k, char *name1, char *owner1)
-{
-char *ownercopy;
-char *namecopy;
-int i1, i2;
-while (name1[i])
-	i++;
-while (owner1[k])
-	k++;
-for (i1 = 0; i1 <= i; i1++)
-	namecopy[i1] = name1[i1];
-while (i1 <= i)
-{
-	namecopy[i1] = '\0';
-	i1++;
-}
-for (i2 = 0; i2 <= k; i2++)
-ownercopy[i2] = owner1[i2];
-while (i2 <= k)
-{
-ownercopy[i2] = '\0';
-i2++;
-}
-}
-/**
  * new_dog - creates a new dog
  * Return: character array
  * @name: variable
@@ -48,6 +17,5 @@ if (p == NULL)
 (*p).name = name;
 (*p).age = age;
 (*p).owner = owner;
-//_strcp(0, 0, &(*p).name, &(*p).owner);
 return (p);
 }
