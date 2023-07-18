36b2aa11fe35f51f2fd83e020fa5966d4c83dc79
chalwemwansa <chalwemwansa23@gmail.com>
2023-07-18 10:55:40 +0200

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,12 +6,12 @@
  * @age: variable to be used
  * @owner: variable to be used
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 #endif
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
