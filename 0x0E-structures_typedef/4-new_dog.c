b26242ba8c83d9fb41a0396b8c1626ff021fe021
chalwemwansa <chalwemwansa23@gmail.com>
2023-07-18 21:29:23 +0200

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * strc - copies a string
+ * Return: a character array
+ * @str: the string to copy
+ * @dest: the destination string
+ */
+char *strc(char *dest, char *str)
+{
+unsigned int i = 0;
+unsigned int j;
+while (str[i] != '\0')
+i++;
+for (j = 0; j < i; j++)
+	dest[j] = str[j];
+dest[j] = '\0';
+return (dest);
+}
+/**
  * new_dog - creates a new dog
  * Return: character array
  * @name: variable
@@ -11,6 +28,10 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *p;
+char *Name;
+char *Owner;
+Name = strc(Name, name);
+Owner = strc(Owner, owner);
 p = malloc(sizeof(dog_t));
 if (p == NULL)
 	return (NULL);
