173c9d3b6260d932560da08138eca531cb75855b
chalwemwansa <chalwemwansa23@gmail.com>
2023-07-21 23:05:02 +0200

    0x0F. C - Function pointers

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ *
+ * Return: 0
+ * @argc: gives the total number of arguments
+ * @argv: the different arguments provided
+ */
+int main(int argc, char *argv[])
+{
+
+	int i, a, b;
+
+	if (*argv[2] == 92 || *argv[2] == 46)
+		printf("Error\n");
+	                exit (99);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit (98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	i = get_op_func(argv[2])(a, b);
+	if (i == 0)
+	{
+		printf("Error\n");
+		exit (99);
+	}
+	else
+		printf("%d\n", i);
+	return (0);
+
+}
