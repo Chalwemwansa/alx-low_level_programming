a6e8e38351c4133b56d2a28c23ddb91fcb579d9e
chalwemwansa <chalwemwansa23@gmail.com>
2023-11-22 22:02:00 +0200

    checking if task 5 is ok

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").my_function.__doc__)'
+    function that computes the perimeter of an island
+"""
+
+
+def island_perimeter(grid):
+    """the function computes the perimeter of an
+        island which is a list of lists with a 1 as
+        land as 0 as sea
+    """
+    if grid is None or len(grid) == 0 or len(grid[0]) == 0:
+        return 0
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            sum = 0
+            if grid[i][j] == 1:
+                try:
+                    tmp = grid[i + 1][j]
+                    if tmp == 0:
+                        sum += 1
+                except IndexError:
+                    pass
+                if i - 1 >= 0:
+                    try:
+                        tmp = grid[i - 1][j]
+                        if tmp == 0:
+                            sum += 1
+                    except IndexError:
+                        pass
+                if j - 1 >= 0:
+                    try:
+                        tmp = grid[i][j - 1]
+                        if tmp == 0:
+                            sum += 1
+                    except IndexError:
+                        pass
+                try:
+                    tmp = grid[i][j + 1]
+                    if tmp == 0:
+                        sum += 1
+                except IndexError:
+                    pass
+                if sum == 4:
+                    if perimeter > sum:
+                        return perimeter
+                    else:
+                        return sum
+                perimeter += sum
+    return perimeter
